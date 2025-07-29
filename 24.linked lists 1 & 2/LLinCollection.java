38c9f751a9c4977899294497863d3d2eaee310af
Harshal Mulay <harshalmulay1039@gmail.com>
2025-07-29 12:34:31 +0530

    linked list code 29july

---
+++ 24.linked lists 1 & 2/LLinCollection.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+public class LLinCollection {
+    public static void main(String [] ar){
+        //create
+        LinkedList<Integer> list=new LinkedList<>();
+
+        //add
+        list.addLast(1);
+        list.addLast(2);
+        list.addFirst(0);
+        
+        System.out.println(list);
+
+        //remove
+        list.removeLast();
+        list.removeFirst();
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+    }
+    
+}
--- 24.linked lists 1 & 2/LinkedList.java
+++ 24.linked lists 1 & 2/LinkedList.java
@@ -244,6 +244,100 @@ public class LinkedList{
          return false;
     }
 
+    //check cycle 
+    public static void removeCycle(){
+        //dectect cycle
+        Node slow=head;
+        Node fast =head;
+        boolean cycle=false;
+        while(fast != null && fast.next != null){
+            slow =slow.next;
+            fast =fast.next.next;
+            if(fast==slow){
+                cycle=true;
+                break;
+            }
+        }
+        if(cycle== false){
+            return;
+        }
+
+        //find meet point
+        slow=head;
+        Node prev=null;
+        while(slow!=fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+
+        //remove cycle
+        prev.next=null;
+    }
+
+
+    //find mid for merge sort
+    private Node getMid(Node head){
+        Node slow=head;
+        Node fast=head.next;
+
+        while(fast != null && fast .next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+    //merge func for sorting
+    private Node merge(Node head1, Node head2){
+        Node mergeLL=new Node(-1);
+        Node temp =mergeLL;
+
+        while(head1 !=null && head2!=null){
+            if(head1.data<=head2.data){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+
+            }else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+
+        while(head1 !=null){
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        while(head2 != null){
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return mergeLL.next;
+    }
+
+    //mearge sort in linked list 
+    public Node mergeSort(Node head){
+        if(head==null || head.next ==null){
+            return head;
+        }
+        //find mid
+        Node mid=getMid(head);
+
+        //left and right ms
+        Node rightHead= mid.next;
+        mid.next=null;
+        Node newLeft= mergeSort(head);
+        Node newRight= mergeSort(rightHead);
+
+        //merge
+        return merge(newLeft,newRight);
+    }
+
+
     public static void main(String [] args){
         /* 
         //new list 
@@ -293,13 +387,35 @@ public class LinkedList{
         */
 
         // linked list 2
-
+/* 
         System.out.println("----Check the cyclic or not -----");
         head=new Node(1);
-        head.next=new Node(2);
+        Node temp=new Node(2);
+        head.next=temp;
+        
         head.next.next=new Node(3);
-        head.next.next.next=head;
+        head.next.next.next=temp;
         System.out.println(isCycle());
+
+        removeCycle();
+        System.out.println(isCycle());
+*/
+
+
+        //Merge Sort
+        LinkedList ll=new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.addFirst(6);
+        ll.print();
+
+        ll.head=ll.mergeSort(ll.head);
+        ll.print();
+
+        
 
     }
 }
