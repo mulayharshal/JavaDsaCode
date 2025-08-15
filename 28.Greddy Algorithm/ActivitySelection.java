8c68d5cccdcbbcb03f81368c921ad9af02a142fa
Harshal Mulay <harshalmulay1039@gmail.com>
2025-08-15 22:32:23 +0530

    today

--- 28.Greddy Algorithm/ActivitySelection.java
+++ 28.Greddy Algorithm/ActivitySelection.java
@@ -6,7 +6,7 @@ public class ActivitySelection {
     public static void main(String[] a) {
         int start[] = { 1, 3, 0, 5, 8, 5 };
         int end[] = { 2, 4, 6, 7, 9, 9 };
-
+ 
 
         // sorting
         int activites[][]=new int [start.length][3];
