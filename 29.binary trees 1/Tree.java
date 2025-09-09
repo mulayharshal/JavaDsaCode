dfcb27f317a762d8bb5dafc516c8c288d9d5968e
Harshal Mulay <harshalmulay1039@gmail.com>
2025-09-09 12:36:43 +0530

    binary tree 9 Sep

--- 29.binary trees 1/Tree.java
+++ 29.binary trees 1/Tree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Tree{
 
     static class Node {
@@ -58,6 +61,37 @@ public class Tree{
             postorder(root.right);
             System.out.print(root.data+" ");
         }
+        
+
+        //level order Traversal
+        public static void levelOrder(Node root){
+            if(root==null){
+                return;
+            }
+            Queue<Node> q=new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while(!q.isEmpty()){
+                Node currNode=q.remove();
+                if(currNode==null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left!=null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right!=null){
+                        q.add(currNode.right);
+                    }
+
+                }
+            }
+        }
     }
     public static void main (String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -73,6 +107,11 @@ public class Tree{
         System.out.println();
         System.out.println("postorder");
         tree.postorder(root);  // print postorder
+
+        System.out.println();
+        System.out.println("-------level order------------");
+        tree.levelOrder(root);
+        System.out.println("----------------------");
     }
 
 }
